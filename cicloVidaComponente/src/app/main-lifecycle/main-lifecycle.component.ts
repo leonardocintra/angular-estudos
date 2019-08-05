ac73e5889d0b0b2ebc05faa7f18a45d92eba187f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-08-05 17:59:18 -0300

    INitial project

---
+++ cicloVidaComponente/src/app/main-lifecycle/main-lifecycle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-lifecycle',
+  templateUrl: './main-lifecycle.component.html',
+  styleUrls: ['./main-lifecycle.component.css']
+})
+export class MainLifecycleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
